055fbabf0e6bc6745a42eb8144c0969283874b49
server29 <seohuibae73@gmail.com>
2020-07-21 14:29:30 +0900

    update util.py

--- util.py
+++ util.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
 
+import os
 import math
+import random
 import numpy as np
+
 import torch
 import torch.optim as optim
 
@@ -93,3 +96,30 @@ def save_model(model, optimizer, opt, epoch, save_file):
     }
     torch.save(state, save_file)
     del state
+
+def set_seed(seed):
+    # for reproducibility. 
+    # note that pytorch is not completely reproducible 
+    # https://pytorch.org/docs/stable/notes/randomness.html  
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.initial_seed() # dataloader multi processing 
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    return None
+
+
+# https://github.com/sehkmg/tsvprint/blob/master/utils.py
+def dict2tsv(res, file_name):
+    if not os.path.exists(file_name):
+        with open(file_name, 'a') as f:
+            f.write('\t'.join(list(res.keys())))
+            f.write('\n')
+
+    with open(file_name, 'a') as f:
+        f.write('\t'.join([str(r) for r in list(res.values())]))
+        f.write('\n')
